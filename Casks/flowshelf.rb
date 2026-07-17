a774d19d98c943572cff435aa7c0e58de27f90b2
Abu monsur md moheuddin kadery <85584637+mahinkadery@users.noreply.github.com>
2026-07-17 17:50:03 +1000

    flowshelf 1.5.0

--- Casks/flowshelf.rb
+++ Casks/flowshelf.rb
@@ -1,6 +1,6 @@
 cask "flowshelf" do
-  version "1.4.0"
-  sha256 "980db655f2281cb85a2a8b61f42b48784c51b6842516f1b5f5c04b395a86ce2f"
+  version "1.5.0"
+  sha256 "8f4e6a1a5b06e9b8553b71ea804cea3e022e503c1b0bb8e57909d6fd5e318074"
 
   url "https://github.com/mahinkadery/FlowShelf/releases/download/v#{version}/FlowShelf-#{version}.dmg"
   name "FlowShelf"
